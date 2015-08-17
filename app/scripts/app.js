e13aae6434cf99658a66df642fd030a28a9f2616
Vishal R <vishivish18@gmail.com>
2015-08-18 00:19:15 +0530

    Cleaning up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,14 +19,7 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
+      
       .otherwise({
         redirectTo: '/'
       });
